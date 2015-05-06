892e191f6bb45d5e9438cb6c3a73bcefffb3c2ca
Jonathan Wilson <jdw7@rice.edu>
2015-05-06 17:27:49 -0500

    Works on pcb

---
+++ Debug/masterNEW.pp
@@ -0,0 +1,9 @@
+# FIXED
+
+masterNEW.obj: ../masterNEW.c
+masterNEW.obj: C:/ti/ccsv5/ccs_base/msp430/include/msp430g2553.h
+masterNEW.obj: C:/ti/ccsv5/ccs_base/msp430/include/in430.h
+
+../masterNEW.c: 
+C:/ti/ccsv5/ccs_base/msp430/include/msp430g2553.h: 
+C:/ti/ccsv5/ccs_base/msp430/include/in430.h: 
--- master.c
+++ master.c
@@ -9,73 +9,109 @@ const char col_array[5] = {BIT0, BIT3, 0x00, BIT6, BIT7};
 const char row_array[5] = {BIT0, BIT1, BIT2, BIT3, BIT4};
 char pwm_array[5][5] = {
 		{10,		0,		0,		0,		0},
-		{0,			10,		0,		0,		0},
-		{0,			0,		 10, 	0,		0},
-		{0,			0,		 0,		10,		0},
-		{0,			0,		 0,		0,		10}
-}; //row,col. Elements go from 0 to led_num_cycles
+		{0,		10,		0,		0,		0},
+		{0,		0,		 10, 	0,		0},
+		{0,		0,		 0,		10,		0},
+		{0,		0,		 0,		0,		10}
+}; //Layer 0 row,col. Elements go from 0 to led_num_cycles
 
-void set_column(int c, int j);
-void clear_all();
+char pwm_array_2[5][5] = {
+		{0,			0,		0,		0,		10},
+		{0,			0,		0,		10,		0},
+		{0,			0,		 10, 	0,		0},
+		{0,			10,		 0,		0,		0},
+		{10,			0,		 0,		0,		0}
+}; //Layer 1
 
 void main(void){
 
 	WDTCTL = WDTPW + WDTHOLD;	// Stop WDT
 
-	P1SEL  =   BIT2    |   BIT4;	//enable UCA0 transmit and clock
-	P1SEL2 =   BIT2    |   BIT4;
+	BCSCTL3 |= LFXT1S_2;					// Set clock source to VLO (only option-- don't have external oscillator)
+	BCSCTL1 |= DIVA_0;						// ACLK is sourced on VLO by default. Run at 12KHz.
+	BCSCTL2 |= SELM_3 + SELS + DIVM_0;   	// SMCLK  = MCLK = VLO = 12KHz
 
+	P1SEL  =   BIT1 	|BIT2    |   BIT4;	//enable UCA0 transmit and clock
+	P1SEL2 =   BIT1 	|	BIT2    |   BIT4;
+
+	P1DIR |= BIT0;
 	P2DIR |= (BIT0 + BIT1 + BIT2 + BIT3 + BIT4);
 
-	BCSCTL3 |= LFXT1S_2;					// Set clock source to VLO
-	BCSCTL2 |= SELM_3 + SELS;          		// SMCLK  = MCLK = VLO = 12KHz
+	UCA0CTL0 |= UCCKPL + UCMSB + UCMST + UCSYNC + UCMODE_1;  // 3-pin, 8-bit SPI master
+	UCA0CTL1 |= UCSSEL_2;                     // SMCLK
+	UCA0BR0 |= 0x02;                          // /2
+	UCA0BR1 = 0;                              //
+	UCA0MCTL = 0;                             // No modulation
+	UCA0CTL1 &= ~UCSWRST;                     // **Initialize USCI state machine**
+	//	  IE2 |= UCA0RXIE;                          // Enable USCI0 RX interrupt
 
-	UCA0CTL1   =   UCSWRST;
-	UCA0CTL0   |=  UCCKPH  +   UCMSB   +   UCMST   +   UCSYNC + UCMODE0; //  4-pin with SS enable high,  8-bit   SPI master
-	UCA0CTL1   |=  UCSSEL_2;   //  SMCLK
-	UCA0BR0    |=  0x00;   //don't prescale Baud rate
-	UCA0BR1    =   0;  //don't prescale Baud rate
-	UCA0MCTL   =   0;  //  No  modulation
-	UCA0CTL1   &=  ~UCSWRST;   //  **Initialize    USCI    state   machine**
-
-
+	int count = 0;	//index of the layer frame we are on in the sequence
 	while(1){
 
 		int c; // global intensity setting, later will be an array with 25 (5x5) elements
 		int r; // cycle index running from 0 to 15
-		int k;
 
 		// set the control array
-		int row,col;
-		int nextRow [5];
-		for (col=0;col<5;col++)
-			nextRow[col] = pwm_array[4][col];
-		for (row=4;row>0;row--){
+
+		//downwards rotate layer 0 array columns
+		if (count % 2== 0){
+			int row,col;
+			int nextRow [5];
 			for (col=0;col<5;col++)
-				pwm_array[row][col] = pwm_array[row-1][col];
+				nextRow[col] = pwm_array[4][col];
+			for (row=4;row>0;row--){
+				for (col=0;col<5;col++)
+					pwm_array[row][col] = pwm_array[row-1][col];
+			}
+			for (col=0;col<5;col++)
+				pwm_array[0][col] = nextRow[col];
 		}
-		for (col=0;col<5;col++)
-			pwm_array[0][col] = nextRow[col];
+		//downwards rotate layer 0 array columns
+		if (count % 2== 1){
+			int row,col;
+			int nextRow [5];
+			for (col=0;col<5;col++)
+				nextRow[col] = pwm_array_2[4][col];
+			for (row=4;row>0;row--){
+				for (col=0;col<5;col++)
+					pwm_array_2[row][col] = pwm_array_2[row-1][col];
+			}
+			for (col=0;col<5;col++)
+				pwm_array_2[0][col] = nextRow[col];
+		}
 
-//		pwm_array[0][1] = 10 - pwm_array[0][1];
-		// set the led matrix to a state for 1000 iterations
+		//Set slave select pin
+		if (count % 2 == 0){
+			P2OUT |= BIT0;
+			P2OUT &= ~BIT1;
+		}
+		else{
+			P2OUT |= BIT1;
+			P2OUT &= ~BIT0;
+		}
 
-		int index;
-		for (index=0; index<5;index++){
-			for (k=0;k<1; k++){
-				P2OUT |= BIT0;
-				for (c=0; c<5; c++){
-					for (r=0; r<5; r++){
-						//PWM all lights
-
-						while  (!(IFG2 &   UCA0TXIFG));    //  USCI_A0 TX  buffer  ready?
-						UCA0TXBUF  =   pwm_array[r][c];   //  Send data byte  over SPI to  Slave. Would use UCA0RXBUF to receive.
-
-		//				_BIS_SR(LPM3_bits);	//enter low-power mode.
-					}
+		//Send layer frame
+		for (r=0; r<5; r++){
+			for (c=0; c<5; c++){
+				//PWM all lights
+				while  (!(IFG2 &   UCA0TXIFG));    //  USCI_A0 TX  buffer  ready?
+				if (count % 2 == 1){
+					UCA0TXBUF = pwm_array_2[r][c];	// send layer 1
 				}
+				else{
+					UCA0TXBUF = pwm_array[r][c];	// send layer 0
+				}
+
+				__delay_cycles(1000);				// delay before writing new byte to transmit buffer
 			}
 		}
+
+		if (count % 2 == 1){
+			__delay_cycles(10);					// delay between Cube Frames
+		}
+
+		count = (count + 1) % 20;					// increment frame index
+
 	}
 
 }
